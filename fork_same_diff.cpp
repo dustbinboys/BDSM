c9fa3c1f65f98cab015ced71b5634bcbb9bcf53c
findme <105403882+dustbinboys@users.noreply.github.com>
2024-12-07 08:58:01 +0530

    Update fork_same_diff.cpp

--- fork_same_diff.cpp
+++ fork_same_diff.cpp
@@ -8,27 +8,27 @@
 
 int main() {
 
-    pid_t pid = fork();  // Create child process
+    pid_t pid = fork();  
 
 
 
-    if (pid == 0) {  // Child process
+    if (pid == 0) {  
 
         std::cout << "This is the child process with PID " << getpid() << std::endl;
 
-        // Child performs a different task
+        
 
         std::cout << "Child: Executing different code!" << std::endl;
 
-    } else {  // Parent process
+    } else {  
 
         std::cout << "This is the parent process with PID " << getpid() << std::endl;
 
-        // Parent performs its task
+        
 
         std::cout << "Parent: Executing different code!" << std::endl;
 
-        wait(NULL);  // Wait for child to finish
+        wait(NULL);  
 
     }
 
